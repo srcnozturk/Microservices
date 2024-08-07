97564deec8bf0ea587d555f0b56cb32a11901c38
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-07 17:02:23 +0300

    CourseController

---
+++ Services/Catalog/Services.Catalog.API/Controllers/CourseController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Catalog.API.Services.Interfaces;
+using Shared.ControllerBases;
+using System.Threading.Tasks;
+
+namespace Services.Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var response = await _courseService.GetAllAsync();
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+            return CreateActionResultInstance(response);
+        }
+
+        [Route("/api/[controller]/GetAllByUserId/{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUserIdAsync(userId);
+            return CreateActionResultInstance(response);
+        }
+
+
+    }
+}
