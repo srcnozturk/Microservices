04dd581fd10094593a018ba2f9d88b7de4462166
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-07 11:28:55 +0300

    Shared Class Library

---
+++ Shared/Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get;private set; }
+
+        [JsonIgnore]
+        public int StatusCode { get; private set; }
+        
+        [JsonIgnore]
+        public bool IsSuccessfull { get; private set; }
+        public List<string> Errors { get; set; }
+
+        // Static Factory Method
+        public static ResponseDto<T> Success(T data, int statusCode)
+        {
+            return new ResponseDto<T> { Data = data, StatusCode = statusCode,IsSuccessfull=true };
+        }
+        public static ResponseDto<T> Success(int statusCode)
+        {
+            return new ResponseDto<T> {  Data=default(T),StatusCode = statusCode, IsSuccessfull = true };
+        }
+
+        public static ResponseDto<T> Fail(int statusCode, List<string> errors)
+        {
+            return new ResponseDto<T>
+            {
+                IsSuccessfull= false,
+                StatusCode = statusCode,
+                Errors = errors
+            };
+        }
+        public static ResponseDto<T> Fail(int statusCode, string errors)
+        {
+            return new ResponseDto<T>
+            {
+                IsSuccessfull = false,
+                StatusCode = statusCode,
+                Errors = new List<string> { errors }
+            };
+        }
+
+    }
+}
