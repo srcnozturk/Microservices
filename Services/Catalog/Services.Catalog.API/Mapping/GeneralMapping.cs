911e2440389f18d5f5e4112ee6a236d5387c3e8d
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-07 11:41:27 +0300

    AutoMapper

---
+++ Services/Catalog/Services.Catalog.API/Mapping/GeneralMapping.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Services.Catalog.API.Dtos;
+using Services.Catalog.API.Models;
+
+namespace Services.Catalog.API.Mapping
+{
+    public class GeneralMapping : Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Course,CourseDto>().ReverseMap();
+            CreateMap<Category,CategoryDto>().ReverseMap();
+            CreateMap<Feature,FeatureDto>().ReverseMap();
+
+            CreateMap<Course,CourseCreateDto>().ReverseMap();
+            CreateMap<Course,CourseUpdateDto>().ReverseMap();
+        }
+    }
+}
