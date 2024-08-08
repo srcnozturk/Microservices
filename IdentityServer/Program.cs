5b147d9d090466c0463412973eaf9533ff034061
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-08 11:20:22 +0300

    identityServer configuration

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -2,7 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer.Data;
+using IdentityServer.Models;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,25 +41,28 @@ namespace IdentityServer
 
             try
             {
-                var seed = args.Contains("/seed");
-                if (seed)
-                {
-                    args = args.Except(new[] { "/seed" }).ToArray();
-                }
-
                 var host = CreateHostBuilder(args).Build();
 
-                if (seed)
+                using (var scope= host.Services.CreateScope())
                 {
-                    Log.Information("Seeding database...");
-                    var config = host.Services.GetRequiredService<IConfiguration>();
-                    var connectionString = config.GetConnectionString("DefaultConnection");
-                    SeedData.EnsureSeedData(connectionString);
-                    Log.Information("Done seeding database.");
-                    return 0;
-                }
+                    var serviceProvider=scope.ServiceProvider;
+                    var applicationDbContext= serviceProvider.GetRequiredService<ApplicationDbContext>();
+                    applicationDbContext.Database.Migrate();
 
-                Log.Information("Starting host...");
+                    var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+                    if(!userManager.Users.Any())
+                    {
+                        userManager.CreateAsync(new ApplicationUser
+                        {
+                            UserName = "sercan",
+                            Email = "sercan@gmail.com",
+                            City = "Tekirdağ",
+
+                        },"Password12*").Wait();
+                    }
+                }
+                    Log.Information("Starting host...");
                 host.Run();
                 return 0;
             }
