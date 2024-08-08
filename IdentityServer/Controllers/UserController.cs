73ae697d30c9a479f42f0a8f5a5110ab28d33f70
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-08 14:04:47 +0300

    SignUp Endpoint

---
+++ IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using IdentityServer.Dtos;
+using IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Dtos;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Signup(SignupDto signupDto)
+        {
+            var user = new ApplicationUser { UserName = signupDto.UserName, Email = signupDto.Email, City = signupDto.City };
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+            if (!result.Succeeded) return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 400));
+        
+            return NoContent();
+        }
+
+    }
+}
