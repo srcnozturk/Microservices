0565c6db6b583e071d6279a4f31817343457d32a
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-08 09:15:21 +0300

    Categories

---
+++ Services/Catalog/Services.Catalog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Catalog.API.Dtos;
+using Services.Catalog.API.Services.Interfaces;
+using Shared.ControllerBases;
+using System.Threading.Tasks;
+
+namespace Services.Catalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return CreateActionResultInstance(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(string id)
+        {
+            var categories =await _categoryService.GetByIdAsync(id);
+            return CreateActionResultInstance(categories);
+        }
+        public async Task<IActionResult> Create(CategoryDto categoryDto)
+        {
+            var category=await _categoryService.CreateAsync(categoryDto);
+            return CreateActionResultInstance(category);
+        }
+    }
+}
--- Services/Catalog/Services.Catalog.API/Services/Interfaces/ICategoryService.cs
+++ Services/Catalog/Services.Catalog.API/Services/Interfaces/ICategoryService.cs
@@ -9,7 +9,7 @@ namespace Services.Catalog.API.Services.Interfaces
     public interface ICategoryService
     {
         Task<Response<List<CategoryDto>>> GetAllAsync();
-        Task<Response<CategoryDto>> CreateAsync(Category category);
+        Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto);
         Task<Response<CategoryDto>> GetByIdAsync(string id);
     }
 }
