b4a05aa0088a2e74bc9a26f13198dd086c108ebc
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-14 13:00:26 +0300

    photoSave endpoint

---
+++ Services/PhotoStock/PhotoStock.API/Controllers/PhotosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhotoStock.API.Dtos;
+using Shared.ControllerBases;
+using Shared.Dtos;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PhotoStock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photo, CancellationToken cancellationToken)
+        {
+            if (photo is null && photo.Length > 0)
+            {
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photo.FileName);
+
+                using var stream = new FileStream(path, FileMode.Create);
+                await photo.CopyToAsync(stream, cancellationToken);
+                
+
+                //http://www.photostock.api.com/photos/asadfa.jpg
+                var returnPath ="photos/"+photo.FileName;
+
+                PhotoDto photoDto = new() { Url=returnPath};
+
+                return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+            }
+            return CreateActionResultInstance(Response<PhotoDto>.Fail("photo is empty", 400));
+        }
+    }
+}
