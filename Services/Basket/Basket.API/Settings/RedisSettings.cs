59454ee85a69995127ec1aa376d42e3736e79b8b
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-14 15:41:45 +0300

    appsettings

---
+++ Services/Basket/Basket.API/Settings/RedisSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Services.Basket.API.Settings
+{
+    public class RedisSettings
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
--- Services/Basket/Basket.API/Startup.cs
+++ Services/Basket/Basket.API/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Services.Basket.API.Settings;
 
 namespace Basket.API
 {
@@ -25,7 +20,7 @@ namespace Basket.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.Configure<RedisSettings>(Configuration.GetSection(nameof(RedisSettings)));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
