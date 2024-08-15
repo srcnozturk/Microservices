7a39d6d181ba476d27545bb1ae99fc04ecfd396d
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-15 15:30:50 +0300

    discountModel

---
+++ Services/Discount/Services.Discount.API/Services/IDiscountService.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Services.Discount.API.Services
+{
+    public interface IDiscountService
+    {
+        //Task<Response<List<Models.Discount> 
+    }
+}
