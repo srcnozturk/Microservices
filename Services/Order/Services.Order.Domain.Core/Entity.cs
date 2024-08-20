c35f691e261fa8c5e62b78f0d6e8422ae55c2eec
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-20 13:36:40 +0300

    entity

--- Services/Order/Services.Order.Domain.Core/Entity.cs
+++ Services/Order/Services.Order.Domain.Core/Entity.cs
@@ -23,8 +23,7 @@ namespace Services.Order.Domain.Core
             if (!IsTransient())
             {
                 if (!_requestedHashCode.HasValue)
-                    _requestedHashCode = this.Id.GetHashCode() ^ 31; // XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
-
+                    _requestedHashCode = this.Id.GetHashCode() ^ 31; 
                 return _requestedHashCode.Value;
             }
             else
