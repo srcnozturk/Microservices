a4a866a301faf40063902310e7d32c8d583968c4
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-20 14:58:53 +0300

    Dto's

---
+++ Services/Order/Services.Order.Application/Dtos/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using Services.Order.Domain.OrderAggregate;
+using System;
+using System.Collections.Generic;
+
+namespace Services.Order.Application.Dtos
+{
+    public class OrderDto
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Address Address { get; set; }
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+    }
+}
