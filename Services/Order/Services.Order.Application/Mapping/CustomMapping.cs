097c913894f6511cf3325ccfdd4b17c02ade0bed
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-20 15:11:00 +0300

    AutoMapper

---
+++ Services/Order/Services.Order.Application/Mapping/CustomMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Services.Order.Application.Dtos;
+using Services.Order.Domain.OrderAggregate;
+
+namespace Services.Order.Application.Mapping
+{
+    public class CustomMapping : Profile
+    {
+        public CustomMapping()
+        {
+            CreateMap<Domain.OrderAggregate.Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+            CreateMap<Address,AddressDto>().ReverseMap();
+        }
+    }
+}
