a2bc9ca43b0eff1475b6534364d95e3da9178861
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-21 10:48:45 +0300

    CreateOrderCommandHandler

---
+++ Services/Order/Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Services.Order.Application.Dtos;
+using Shared.Dtos;
+using System.Collections.Generic;
+
+namespace Services.Order.Application.Commands
+{
+    class CreateOrderCommand : IRequest<Response<CreatedOrderDto>>
+    {
+        public string BuyerId { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public AddressDto AddressDto { get; set; }
+    }
+}
---
+++ Services/Order/Services.Order.Application/Handlers/CreateOrderCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Services.Order.Application.Commands;
+using Services.Order.Application.Dtos;
+using Services.Order.Domain.OrderAggregate;
+using Services.Order.Infrastructure;
+using Shared.Dtos;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Services.Order.Application.Handlers
+{
+    class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand, Response<CreatedOrderDto>>
+    {
+        private readonly OrderDbContext db;
+
+        public CreateOrderCommandHandler(OrderDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<Response<CreatedOrderDto>> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var newAddress = new Address(request.AddressDto.Province, request.AddressDto.District, request.AddressDto.Street, request.AddressDto.ZipCode,
+                request.AddressDto.Line);
+
+            Domain.OrderAggregate.Order order = new Domain.OrderAggregate.Order(request.BuyerId, newAddress);
+
+            request.OrderItems.ForEach(x =>
+            {
+                order.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
+
+            await db.Orders.AddAsync(order);
+            await db.SaveChangesAsync();
+
+            return Response<CreatedOrderDto>.Success(new CreatedOrderDto { OrderId = order.Id },200);
+        }
+    }
+}
