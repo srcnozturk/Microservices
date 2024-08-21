2ca58104913f9b8b84b20a9fd6295c7864529ade
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-21 13:57:39 +0300

    Dtosnames changed

--- Services/Order/Services.Order.Application/Commands/CreateOrderCommand.cs
+++ Services/Order/Services.Order.Application/Commands/CreateOrderCommand.cs
@@ -9,6 +9,6 @@ namespace Services.Order.Application.Commands
     {
         public string BuyerId { get; set; }
         public List<OrderItemDto> OrderItems { get; set; }
-        public AddressDto AddressDto { get; set; }
+        public AddressDto Address { get; set; }
     }
 }
