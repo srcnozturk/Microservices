b11828cee783d387c280b6114baecefe2e3c6c80
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-21 15:58:55 +0300

    catelog api category add default

--- Services/Catalog/Services.Catalog.API/Program.cs
+++ Services/Catalog/Services.Catalog.API/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Services.Catalog.API.Dtos;
+using Services.Catalog.API.Models;
+using Services.Catalog.API.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +17,17 @@ namespace Services.Catalog.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+          var host=   CreateHostBuilder(args).Build();
+            using (var scope=host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var categoryService = services.GetRequiredService<ICategoryService>();
+                if(!categoryService.GetAllAsync().Result.Data.Any())
+                {
+                    categoryService.CreateAsync(new CategoryDto { Name="Asp.Net Core Kursu"}).Wait();
+                    categoryService.CreateAsync(new CategoryDto { Name="Asp.Net Core API Kursu"}).Wait();
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
