96e35884a73d0f7ece2c6d221ea2b26648124fb4
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-22 15:49:48 +0300

    ServiceApiSettings

--- FrontEnds/Course.Web/Startup.cs
+++ FrontEnds/Course.Web/Startup.cs
@@ -1,12 +1,9 @@
+using Course.Web.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Course.Web
 {
@@ -22,6 +19,7 @@ namespace Course.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
             services.AddControllersWithViews();
         }
 
