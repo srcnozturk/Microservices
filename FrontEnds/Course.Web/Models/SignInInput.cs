31c00dcabfd0205194a79fd4fcb615edbb806052
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-22 15:16:50 +0300

    IIdentityService

---
+++ FrontEnds/Course.Web/Models/SignInInput.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course.Web.Models
+{
+    public class SigninInput
+    {
+        [Display(Name ="Email adresiniz")]
+        public string Email { get; set; }
+        [Display(Name ="Şifreniz")]
+        public string Password { get; set; }
+        [Display(Name ="Beni hatırla")]
+        public bool IsRemember { get; set; }
+
+    }
+}
