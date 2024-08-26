b4c9d8a2adc7c078bc74edd9a565640f8a979235
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-08-26 16:06:06 +0300

    UserService/GetUser

--- FrontEnds/Course.Web/Services/UserService.cs
+++ FrontEnds/Course.Web/Services/UserService.cs
@@ -1,14 +1,23 @@
 ﻿using Course.Web.Models;
 using Course.Web.Services.Interfaces;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Threading.Tasks;
 
 namespace Course.Web.Services
 {
     public class UserService : IUserService
     {
-        public Task<UserViewModel> GetUser()
+        private readonly HttpClient _httpClient;
+
+        public UserService(HttpClient httpClient)
         {
-            throw new System.NotImplementedException();
+            _httpClient = httpClient;
+        }
+
+        public async Task<UserViewModel> GetUser()
+        {
+            return await _httpClient.GetFromJsonAsync<UserViewModel>("/api/user/getuser");
         }
     }
 }
