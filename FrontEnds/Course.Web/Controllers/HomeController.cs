79f3f846ed361e811814a80967bd40b2484d20ea
srcn.ozturk <sercan_ozturk59@hotmail.com>
2024-09-03 11:23:23 +0300

    HomeController

--- FrontEnds/Course.Web/Controllers/HomeController.cs
+++ FrontEnds/Course.Web/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
 ﻿using Course.Web.Models;
+using Course.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Course.Web.Controllers
@@ -12,22 +10,19 @@ namespace Course.Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ICatalogService _catalogService;
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _catalogService.GetAllCourseAsync());
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+    
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
