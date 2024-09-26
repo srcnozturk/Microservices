c8f01fb16e165a500560ffacab71973d159f0e35
srcnozturk <sercan_ozturk59@hotmail.com>
2024-09-26 13:42:35 +0300

    readme updated

---
+++ FrontEnds/Course.Web/Models/Baskets/BasketItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Course.Web.Models.Baskets
+{
+    public class BasketItemViewModel
+    {
+        public int Quantity { get; set; }
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal Price { get; set; }
+    }
+}
