ec796fa6a32615e8a8a18e426f8740abbde91989
srcnozturk <sercan_ozturk59@hotmail.com>
2024-09-27 16:40:50 +0300

    DiscountModels

---
+++ FrontEnds/Course.Web/Validators/DiscountApplyInputValidator.cs
@@ -0,0 +1,13 @@
+﻿using Course.Web.Models.Discount;
+using FluentValidation;
+
+namespace Course.Web.Validators
+{
+    public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput>
+    {
+        public DiscountApplyInputValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty().WithMessage("İndirim alanı boş bırakılamaz!");
+        }
+    }
+}
