56f1d69e65ac21c99a11996f4be3a2b31a33186e
srcnozturk <sercan_ozturk59@hotmail.com>
2024-11-05 19:29:28 +0300

    Basket bug

--- FrontEnds/Course.Web/Models/Baskets/BasketViewModel.cs
+++ FrontEnds/Course.Web/Models/Baskets/BasketViewModel.cs
@@ -6,10 +6,14 @@ namespace Course.Web.Models.Baskets
 {
     public class BasketViewModel
     {
+        public BasketViewModel()
+        {
+            _basketItems = new List<BasketItemViewModel>();
+        }
         public string UserId { get; set; }
         public string DiscountCode { get; set; }
         public int? DiscountRate { get; set; }
-        private List<BasketItemViewModel> _basketItems { get; set; }
+        private List<BasketItemViewModel> _basketItems;
 
         public List<BasketItemViewModel> BasketItems
         {
