1fa5eaf059e12dd3eff0f76a4222b9c6b19c59e5
srcnozturk <sercan_ozturk59@hotmail.com>
2024-11-06 16:25:16 +0300

    Checkout.cshtml && OrderController

---
+++ FrontEnds/Course.Web/Controllers/OrderController.cs
@@ -0,0 +1,45 @@
+﻿using Course.Web.Models.Orders;
+using Course.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Course.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IBasketService _basketService;
+        private readonly IOrderService _orderService; 
+
+        public OrderController(IBasketService basketService, IOrderService orderService)
+        {
+            _basketService = basketService;
+            _orderService = orderService;
+        }
+
+        public async Task<IActionResult> CheckOut()
+        {
+            var basket = await _basketService.Get();
+            ViewBag.basket=basket;
+            return View(new CheckoutInfoInput());
+        }
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CheckoutInfoInput checkoutInfoInput)
+        {
+            var orderStatus = await _orderService.CreateOrder(checkoutInfoInput);
+            if (!orderStatus.IsSuccessfull)
+            {
+                var basket = await _basketService.Get();
+                ViewBag.basket = basket;
+                ViewBag.error = orderStatus.Error;
+                return View();
+            }
+            return View(nameof(SuccessfullCheckout), new {orderId=orderStatus.OrderId}) ;
+        }
+
+        public IActionResult SuccessfullCheckout(int orderId)
+        {
+            ViewBag.orderId = orderId;
+            return View();
+        }
+    }
+}
