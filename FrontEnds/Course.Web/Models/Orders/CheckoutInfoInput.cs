3cd2f1f7635ff356e83af5d5ff62019dad76a6ee
srcnozturk <sercan_ozturk59@hotmail.com>
2024-11-06 14:06:41 +0300

    CheckoutInfoInputModel

---
+++ FrontEnds/Course.Web/Models/Orders/CheckoutInfoInput.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Course.Web.Models.Orders
+{
+    public class CheckoutInfoInput
+    {
+        [Display(Name ="İl")]
+        public string Province { get; set; }
+        [Display(Name = "İlçe")]
+        public string District { get; set; }
+        [Display(Name = "Cadde")]
+        public string Street { get; set; }
+        [Display(Name = "Posta Kodu")]
+        public string ZipCode { get; set; }
+        [Display(Name = "Adres")]
+        public string Line { get; set; }
+        [Display(Name = "Kart isim Soyisim")]
+        public string CardName { get; set; }
+        [Display(Name = "Kart Numarası")]
+        public string CardNumber { get; set; }
+        [Display(Name = "Son Kullanma Tarihi(Ay/Yıl)")]
+        public string Expiration { get; set; }
+        [Display(Name = "CVV/CVC Kodu")]
+        public string CVV { get; set; }
+    }
+}
