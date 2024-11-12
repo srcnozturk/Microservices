6b9cefa7aa8150c4cfbf6c4a13e974a3c8e17d8e
srcnozturk <sercan_ozturk59@hotmail.com>
2024-11-12 13:32:53 +0300

    rabbitMQ Order db save test

---
+++ Services/Order/Services.Order.Application/Consumer/CreateOrderMessageCommandConsumer.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using Services.Order.Infrastructure;
+using Shared.Messages;
+using System.Threading.Tasks;
+
+namespace Services.Order.Application.Consumer
+{
+    public class CreateOrderMessageCommandConsumer : IConsumer<CreateOrderMessageCommand>
+    {
+
+        private readonly OrderDbContext _orderDbContext;
+
+        public CreateOrderMessageCommandConsumer(OrderDbContext context)
+        {
+            _orderDbContext = context;
+        }
+
+        public async Task Consume(ConsumeContext<CreateOrderMessageCommand> context)
+        {
+            var newAddress = new Domain.OrderAggregate.Address(
+                context.Message.Province, context.Message.District,
+                context.Message.Street, context.Message.ZipCode, context.Message.Line);
+
+            Domain.OrderAggregate.Order order = new Domain.OrderAggregate.Order(context.Message.BuyerId, newAddress);
+
+            context.Message.OrderItems.ForEach(x =>
+            {
+                order.AddOrderItem(x.ProductId, x.ProductName, x.Price, x.PictureUrl);
+            });
+
+            await _orderDbContext.Orders.AddAsync(order);
+            await _orderDbContext.SaveChangesAsync();
+        }
+    }
+}
