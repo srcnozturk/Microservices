91eae81b0e0ab1cac8b4a12a8c22e160119a327c
srcnozturk <sercan_ozturk59@hotmail.com>
2024-11-19 16:09:54 +0300

    TokenExchangeDelegateHandler config

--- Gateway/Gateway.API/Startup.cs
+++ Gateway/Gateway.API/Startup.cs
@@ -1,3 +1,4 @@
+using Gateway.API.DelegateHandlers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,13 +22,14 @@ namespace Gateway.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<TokenExchangeDelegateHandler>();
             services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
             {
                 options.Authority = Configuration["IdentityServerURL"];
                 options.Audience = "resource_gateway";
                 options.RequireHttpsMetadata = false;
             });
-            services.AddOcelot();
+            services.AddOcelot().AddDelegatingHandler<TokenExchangeDelegateHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
